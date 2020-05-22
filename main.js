7043880f437cdd6e414b7cfddc3490759413c439
Cindy Smith <cmsmith43152@gmail.com>
2020-05-21 20:27:56 -0400

    submitting something

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 const rollButton = document.querySelector('#roll-button')
+const rollNumber = document.querySelector('')
 
-const dieRolls = []
+let dieRolls = []
+let total = 0
+let randnum
 
-rollButton.addEventListener("click", function()) {
 
-}
+rollButton.addEventListener("click", function() {
+
+    console.log("roll-button")
+
+    let numberOfrolls = rollNumber.value
+    console.log (numberOfrolls)
+
+    //loop a number of times equal to the number-of-dice//
+    //up to on click, you need to get the value from the text input box//
+    //get the value of the input text box and assign it//
+    // loop that number of times. 
+    // and each time "roll" randomly from 1 to 6
+    //loop a number of times equal to the number-of-dice
+    
+
+
+})
