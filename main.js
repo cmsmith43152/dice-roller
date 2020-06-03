435c11905812e1f15bef726b51a6ca29047f3ee7
Cindy Smith <cmsmith43152@gmail.com>
2020-06-03 17:10:52 -0400

    finally!

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
-const rollButton = document.querySelector('#roll-button')
-const rollNumber = document.querySelector('Roll!')
+const rollButton = document.querySelector('#RolltheDice')
+const rollNumber = document.querySelector('#numberOfRolls')
+const TotalElement = document.querySelector('#total')
+const ShowAllButton = document.querySelector('#ShowAll')
+const AllRollsElement = document.querySelector('#AllRolls')
+
 
 let dieRolls = []
 let total = 0
-var = []
 
 
-rollButton.addEventListener("click", function() {
+rollButton.addEventListener("click", function () {
 
     console.log("roll-button")
 
     let numberOfrolls = rollNumber.value
-    console.log (numberOfrolls)
+    console.log(numberOfrolls)
 
-    //loop a number of times equal to the number-of-dice//
-    //up to on click, you need to get the value from the text input box//
-    //get the value of the input text box and assign it//
-    // loop that number of times. 
-    // and each time "roll" randomly from 1 to 6
-    //loop a number of times equal to the number-of-dice
-    
+    for (let counter = 0; counter < numberOfrolls; counter += 1) {
+        let currentRoll = Math.floor(Math.random() * 6) + 1
+        dieRolls.push(currentRoll)
+        total = total + currentRoll
+    }
+    console.log(dieRolls)
+    console.log(total)
+    TotalElement.innerHTML = total
+
+
+
+})
+ShowAllButton.addEventListener("click", function () {
+    for (let counter = 0; counter < dieRolls.length; counter += 1) {
+
+        let listElement = "<li class='dice'>" + dieRolls[counter] + "</li>"
+        AllRollsElement.innerHTML += listElement
+    }
+
 
 
 })
